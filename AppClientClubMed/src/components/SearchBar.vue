1d1eee1446c231098f91b5283ea6d51abcdd428e
soudant <Thomas.Soudan@etu.univ-smb.fr>
2023-03-07 17:07:57 +0100

    installation tailwind + compenent Header

---
+++ AppClientClubMed/src/components/SearchBar.vue
@@ -0,0 +1,8 @@
+  <script setup lang="ts">
+  import logoClubMed from './icons/logoClubMed.vue'
+  import iconUser from './icons/iconUser.vue'
+  </script>
+  
+  <template>
+  </template>
+  
--- AppClientClubMed/src/views/HomeView.vue
+++ AppClientClubMed/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/Header.vue'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Header />
   </main>
 </template>
