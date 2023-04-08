6d8546b46458f4d8f5b408020081a6c4a7bd6a41
dasmathe <da.matheo@hotmail.com>
2023-04-08 20:12:45 +0200

    clean rapide

--- AppClientClubMed/src/components/NavBar.vue
+++ AppClientClubMed/src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
+        <router-link to="/" class="-m-1.5 p-1.5">
           <span class="sr-only">Club Med</span>
           <logoClubMed class="h-8 w-auto text-primary-blue" />
-        </a>
+        </router-link>
       </div>
       <div class="flex lg:hidden">
         <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
